d290d906351d41edea667bc3f5d585969a2c76b1
Hilton Meyer <development@hiltonmeyer.com>
2019-09-05 14:50:58 +0300

    Added attributes for markdown

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,36 @@
-module.exports = function(eleventyConfig)  {
-    eleventyConfig.addPassthroughCopy("css");
-  
-    return {
-      passthroughFileCopy: true,
-      dir: {
-        input: ".",
-        includes: "_includes",
-        data: "_data",
-        output: "_site"
-      }
-    
-    };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+
+  /**************** Markdown Plugins********************/
+  let markdownIt = require("markdown-it");
+  var markdownItAttrs = require('markdown-it-attrs');
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
   };
+  let markdownLib = markdownIt(options).use(markdownItAttrs);
+  eleventyConfig.setLibrary("md", markdownLib);
+
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+
+  };
+  /**************** END Markdown Plugins********************/
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+
+  };
+};
