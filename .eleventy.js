da368470bc089089c63e7f113ca41b3aa0fbe555
Hilton Meyer <development@hiltonmeyer.com>
2019-10-03 05:43:34 +0300

    Merge from feature-add-latest-post-to-home

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,15 @@ module.exports = function(eleventyConfig) {
     }).toFormat("dd LLL yyyy");
   });
 
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", dateObj => {
     return DateTime.fromJSDate(dateObj, {
